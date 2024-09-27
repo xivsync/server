300a412b46ed5945caec332f488b4b42bf7e96ce
Bluefissure <bluefissureofg@gmail.com>
2024-09-27 18:38:16 -0500

    Update docker-build-authservice.sh

--- Docker/build/linux-local/docker-build-authservice.sh
+++ Docker/build/linux-local/docker-build-authservice.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../../../
-docker build -t darkarchon/mare-synchronos-server:latest . -f Docker/build/Dockerfile-MareSynchronosAuthService --no-cache --pull --force-rm
+docker build -t darkarchon/mare-synchronos-authservice:latest . -f Docker/build/Dockerfile-MareSynchronosAuthService --no-cache --pull --force-rm
 cd Docker/build/linux-local
