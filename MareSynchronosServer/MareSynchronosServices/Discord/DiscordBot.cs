8494b01bd4cefa2867ec00348476329ffee838c0
Tsubasahane <wozaiha@gmail.com>
2025-01-11 02:00:01 +0800

    Update DiscordBot.cs

--- MareSynchronosServer/MareSynchronosServices/Discord/DiscordBot.cs
+++ MareSynchronosServer/MareSynchronosServices/Discord/DiscordBot.cs
@@ -41,7 +41,7 @@ internal class DiscordBot : IHostedService
         _discordClient = new(new DiscordSocketConfig()
         {
             DefaultRetryMode = RetryMode.AlwaysRetry,
-            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers
+            //GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers
         });
 
         _discordClient.Log += Log;
