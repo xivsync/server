f56b05585e620d2541658da7a8a1afee712fed9d
Tsubasahane <wozaiha@gmail.com>
2025-01-15 09:19:18 +0800

    Debug

--- MareSynchronosServer/MareSynchronosAuthService/Controllers/OAuthController.cs
+++ MareSynchronosServer/MareSynchronosAuthService/Controllers/OAuthController.cs
@@ -291,6 +291,7 @@ public class OAuthController : AuthControllerBase
             string primaryUid = HttpContext.User.Claims.Single(c => string.Equals(c.Type, MareClaimTypes.Uid, StringComparison.Ordinal))!.Value;
             if (string.IsNullOrEmpty(requestedUid)) return BadRequest("无 UID");
             if (string.IsNullOrEmpty(charaIdent)) return BadRequest("无 CharaIdent");
+            if (!string.IsNullOrEmpty(machineId)) Logger.LogInformation(machineId);
 
             var ip = HttpAccessor.GetIpAddress();
 
