acbc640dc46a1b288a4d35829ee3d1a52eb4bf37
Tsubasahane <wozaiha@gmail.com>
2025-01-21 10:51:59 +0800

    Moodles

---
+++ MareSynchronosServer/MareSynchronosShared/Migrations/20250121025026_Moodles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MareSynchronosServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Moodles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "moodles_data",
+                table: "chara_data",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "moodles_data",
+                table: "chara_data");
+        }
+    }
+}
